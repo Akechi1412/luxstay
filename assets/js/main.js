adaa4e5638c94ae6b0d3c00b0d13dc53835b886f
Akechi1412 <nguyenphong10042002@gmail.com>
2022-01-08 20:45:24 +0700

    explore section

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,7 +86,7 @@ $(".suggest__card-list").slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 1,
         arrows: true,
         variableWidth: true,
       },
@@ -102,7 +102,7 @@ $(".suggest__card-list").slick({
   ],
 });
 
-$(".discover__card-list").slick({
+$(".explore__card-list").slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   arrows: true,
